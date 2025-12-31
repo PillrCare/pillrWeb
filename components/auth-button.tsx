4b7d96b660c5bd6be3822a67d8d8d4a137f51646
isnielsen <isnielsen13@gmail.com>
2025-12-31 10:36:14 -0700

    Remove user greeting from auth button
    Simplified auth button UI by removing the 'Hey, user!' greeting text

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -14,7 +14,6 @@ export async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email ? user.email.split('@')[0] : null}!
       <LogoutButton />
     </div>
   ) : (
