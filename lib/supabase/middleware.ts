1b2cfb149840eec1d106410490d133a73b66ecf1
Carson Bentley <carsonbentley7@gmail.com>
2026-02-17 11:17:23 -0700

    no auth for privacy policys

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -53,7 +53,9 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/contact") &&
-    !request.nextUrl.pathname.startsWith("/api/contact")
+    !request.nextUrl.pathname.startsWith("/api/contact") &&
+    !request.nextUrl.pathname.startsWith("/privacy") &&
+    !request.nextUrl.pathname.startsWith("/terms-and-conditions")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
